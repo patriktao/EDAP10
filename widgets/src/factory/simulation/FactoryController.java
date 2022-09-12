4e4a25a6caca24fc61ab076b53c392f4f787f906
Patrik <patriktao@gmail.com>
2022-09-12 17:30:34 +0200

    Busy Commit

--- widgets/src/factory/simulation/FactoryController.java
+++ widgets/src/factory/simulation/FactoryController.java
@@ -9,25 +9,29 @@ public class FactoryController {
 	static class Monitor {
 		Conveyor conv; 
 		boolean paint_finished;
+		boolean press_finished;
 		
 		public Monitor (Conveyor conv) {
 			this.conv = conv;
 			this.paint_finished = false;
+			this.press_finished = false;
 		}
 		
 		public synchronized void pressConvOn() throws InterruptedException{
-			while(paint_finished == false) { //vänta på att paint ska vara klar, jag sovar medans.
+			while(paint_finished || press_finished) { //vänta på att paint ska vara klar, jag sovar medans.
 				 wait();
 			}
 			conv.on(); //fortsätt bandet
 		}
 		
-		public synchronized void paintConvOn() throws InterruptedException{
+		public synchronized void setBusyPainting(){
+			paint_finished = !paint_finished;
 			notifyAll(); //Paint är klar, väck press!
 		}
 		
-		public synchronized void paintFinished(boolean bool) {
-			paint_finished = bool;
+		public synchronized void setBusyPressing(){
+			press_finished = !press_finished;
+			notifyAll();
 		}
 		
 		public synchronized void convOff() {
@@ -40,8 +44,10 @@ public class FactoryController {
 		while (true) {
 			try {
 				press.waitFor(Widget.GREEN_BLOB);
+				mon.setBusyPressing();
 				mon.convOff();
 				press.performAction();
+				mon.setBusyPressing();
 				mon.pressConvOn(); //Vi väntar på att paint ska vara färdigt och sen kan vi köra bandet
 			} catch (InterruptedException e) {
 				throw new Error(e);
@@ -54,10 +60,11 @@ public class FactoryController {
 		while (true) {
 			try {
 				paint.waitFor(Widget.ORANGE_MARBLE);
+				mon.setBusyPainting();
 				mon.convOff(); //Stannar bandet 
 				paint.performAction();
-				mon.paintFinished(true); //ändrar villkoret
-				mon.paintConvOn(); //notifyar pressen att vi är klara!
+				mon.setBusyPainting();
+				mon.pressConvOn();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
