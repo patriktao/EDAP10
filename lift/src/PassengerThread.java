05fcf49bdbb1ffd66c82156034c88e19cd7645af
Patrik <patriktao@gmail.com>
2022-09-22 23:25:24 +0200

    While true loop in creating passengers

--- lift/src/PassengerThread.java
+++ lift/src/PassengerThread.java
@@ -12,18 +12,20 @@ public class PassengerThread extends Thread{
 	
 	public void run() {
 		try {
-			//CREATE PASSENGER
-			Passenger pass = view.createPassenger();
-			//ENTER
-			pass.begin();
-			mon.enter(pass.getStartFloor());
-			pass.enterLift();
-			mon.notify_entered();
-			//EXIT
-			mon.exit(pass.getDestinationFloor()); 
-			pass.exitLift();
-			mon.notify_exited();
-			pass.end();
+			while(true) {
+				//CREATE PASSENGER
+				Passenger pass = view.createPassenger();
+				//ENTER
+				pass.begin();
+				mon.enter(pass.getStartFloor());
+				pass.enterLift();
+				mon.notify_entered();
+				//EXIT
+				mon.exit(pass.getDestinationFloor()); 
+				pass.exitLift();
+				mon.notify_exited();
+				pass.end();				
+			}
 		} catch (InterruptedException e) {
 			throw new Error(e);
 		}
